0d996d49adb9f474b6a1fb933a668b1287aaaa8a
Andrey Sherbakov <asch1998@gmail.com>
2020-04-17 00:31:51 +0300

    Добавьте файлы проекта.

---
+++ homework_14.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name("HelloSkillbox!");
+
+    std::cout << name << "\n" << "\n";
+    std::cout << "string lenght: " << name.length() << "\n";
+    std::cout << "first symbol: " << name[0] << "\n";
+    std::cout << "last symbol: " << name[name.size() - 1];
+
+    return 0;
+}
